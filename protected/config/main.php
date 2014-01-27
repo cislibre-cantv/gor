372a543493a6b5f58c2b3832cd0b2b3565c9c7a1
verenzuela <verenzuela@gmail.com>
2014-01-27 11:13:44 -0430

    fixes #15 - variables name y adminemail

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'GOR - Gestión Operativa Regional',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -87,7 +87,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'cislibre@cantv.com.ve',
 	),
     
         
